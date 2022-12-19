ad4450d58e0345228a7aca1e98961c3970a7a64b
Vito Stamatti <48925565+vitostamatti@users.noreply.github.com>
2022-12-19 12:36:57 +0000

    dummy auth logic

--- BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
 using BubberDinner.Contracts.Authentication;
+using BuberDinner.Application.Services.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
@@ -7,15 +8,52 @@ namespace BuberDinner.Api.Controllers;
 [Route("auth")]
 public class AuthenticationController : ControllerBase
 {
+
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest request)
     {
-        return Ok(request);
+        var authResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password
+        );
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token
+        );
+
+
+        return Ok(response);
     }
 
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request)
     {
-        return Ok(request);
+        var authResult = _authenticationService.Login(
+            request.Email,
+            request.Password
+        );
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token
+        );
+
+        return Ok(response);
     }
 }
--- BuberDinner/BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -2,9 +2,8 @@ namespace BuberDinner.Application.Services.Authentication;
 
 public interface IAuthenticationService
 {
-    AuthenticationResponse Login(LoginRequest request);
+    AuthenticationResult Register(string firstName, string lastName, string email, string password);
+    AuthenticationResult Login(string email, string password);
 
-    AuthenticationResponse Register(RegisterRequest request);
-    
 }
 
